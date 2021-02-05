a1b295979030d777368c84cdae455c0bae86eeb4
d-taylor6403 <dannielletaylor@outlook.com>
2021-02-04 19:49:39 -0600

    adding colors package

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const colors = require('colors');
 
+//Creating array from index 2 of user input
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
-
-
-
+//print array indexes starting at index 2 nad printing to console
 const printProfileData = profileDataArr => {
     console.log("================".magenta);
 

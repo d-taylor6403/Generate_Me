41d76228ea5260833526a09dd6eebf2a79e4adb4
d-taylor6403 <dannielletaylor@outlook.com>
2021-02-08 16:29:03 -0600

    adding inquirer package

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+const inquirer = require('inquirer');
 const colors = require('colors');
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-//Creating array from index 2 of user input
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-const pageHTML = generatePage(name, github);
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
 
-fs.writeFile('index.html', pageHTML, err => {
-    if (err) throw new Error(err);
+//     console.log('Portfolio complete! Check out index.html to see the output!'.magenta);
+// });
 
-    console.log('Portfolio complete! Check out index.html to see the output!'.magenta);
-});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
